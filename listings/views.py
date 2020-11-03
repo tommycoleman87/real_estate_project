4ad1e97bd52e3b78f63da4e7ca8936d94b3f3a50
Tommy Coleman <tommiilee@gmail.com>
2020-11-02 20:40:28 -0500

    added listings urls in django

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return render(request, 'listings/listings.html')
 
-# Create your views here.
+def listing(request):
+    return render(request, 'listings/listing.html')
+
+def search(request):
+    return render(request, 'listings/search.html')
+
+
