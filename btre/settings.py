9be663d47be66925d31484f66f2baeae1160c807
Tommy Coleman <tommiilee@gmail.com>
2020-11-03 14:01:52 -0500

    added postgres and database, ran migrations

--- btre/settings.py
+++ btre/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'btredb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'C0l3m@t0',
+        'HOST' : 'localhost',
+      
     }
 }
 
