e1cf094040860cc51965aa146898c623f17becc2
Tommy Coleman <tommiilee@gmail.com>
2020-11-14 13:59:57 -0500

    created realtor models

--- listings/models.py
+++ listings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from realtors.models import Realtor
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
@@ -23,3 +24,6 @@ class Listing(models.Model):
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.tile
