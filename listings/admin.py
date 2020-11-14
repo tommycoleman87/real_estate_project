703938174f4c42af186d308195856dd384e3d540
Tommy Coleman <tommiilee@gmail.com>
2020-11-14 15:13:23 -0500

    added filter and search functionality to admin page

--- listings/admin.py
+++ listings/admin.py
@@ -4,4 +4,8 @@ from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
 admin.site.register(Listing, ListingAdmin)
