7bd91f4ae1b325927f4923f3d80c3525f5b2de5c
Tommy Coleman <tommiilee@gmail.com>
2020-11-21 15:28:52 -0500

    imported listings from db to html

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Listing
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
