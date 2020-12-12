849dabcde4fdfdb30af4fdc278c2f88b01ef323e
Tommy Coleman <tommiilee@gmail.com>
2020-12-11 20:16:55 -0500

    fixed typo

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from listings.models import 
+from listings.models import Listing
 def index(request):
     return render(request, 'pages/index.html')
 
