2388c8b267f861752502379e6cf60dc3dade26be
Tommy Coleman <tommiilee@gmail.com>
2020-12-11 20:16:31 -0500

    import listing

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from listings.models import Listing
+from listings.models import 
 def index(request):
     return render(request, 'pages/index.html')
 
